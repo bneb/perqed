2aa3be246e955eb6ff7e85adfd6058e989ade9e6
Kevin <oconn.kv@gmail.com>
2026-05-11 21:22:24 -0700

    docs: finalize conclusion to accurately report beta<=3 absolute bound

--- agent_workspace/lakefile.lean
+++ agent_workspace/lakefile.lean
@@ -7,7 +7,7 @@ package «agent_workspace» where
 @[default_target]
 lean_lib «agent_workspace» where
   srcDir := "."
-  roots := #[`problem_statement, `residual_growth_bound, `erdos265_main, `dual_constraint_collapse, `sylvester_irrational, `universal_balance, `subgreedy_bounds]
+  roots := #[`problem_statement, `residual_growth_bound, `erdos265_main, `dual_constraint_collapse, `sylvester_irrational, `universal_balance, `subgreedy_bounds, `fundamental_inequality, `subgreedy_asymptotics, `absolute_upper_bound]
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.6.0"
